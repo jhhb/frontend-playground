6842a81257839419dcd7045053adcf10967f774e
James Boyle <jhhboyle@gmail.com>
2019-08-04 16:56:20 -0400

    Add lodash, lodash-computed div

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import {Button} from '@blueprintjs/core';
 import * as d3 from 'd3';
+import * as _ from 'lodash';
 
 import './App.scss';
+import {Dictionary} from 'lodash';
+
+interface DumbInterface {
+  [key: string]: string
+}
 
 export class App extends Component {
   private node: SVGSVGElement | null = null;
@@ -11,13 +17,19 @@ export class App extends Component {
     this.runD3();
   }
 
+  runLodash(): string {
+    const dumbInterfaces: DumbInterface[] = [{key: 'lodash'}, {key: 'works'}]
+    const dict: Dictionary<DumbInterface[]> = _.groupBy(dumbInterfaces, 'key')
+    return Object.keys(dict).join(' ');
+  }
+
   runD3() {
     if (this.node) {
       d3
         .select(this.node)
         .append('text')
           .attr("x", 0)
-          .attr("y", "20")          
+          .attr("y", "20")
           .attr("font-size", "20px")
           .text("d3 works.")
     }
@@ -28,6 +40,7 @@ export class App extends Component {
       <div className="App">
         <Button text="Blueprint works."/>
         <svg ref={node => this.node = node}/>
+        <div>{this.runLodash()}</div>
       </div>
     );
   }
